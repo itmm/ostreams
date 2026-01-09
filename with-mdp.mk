a397db9e3937ac5cf3a704b19ad1ab3633c168ed
Timm Knape <t.knape@arz-darmstadt.de>
2026-01-09 11:38:03 +0100

    besseres Kopieren der Dateien

--- with-mdp.mk
+++ with-mdp.mk
@@ -5,6 +5,7 @@ $(GENERATED): .mdp_run
 
 .mdp_run: $(wildcard *.md)
 	chmod u+w $(GENERATED)
+	$(warning mdp not installed)
 	[ -x "$$(command -v mdp)" ] || echo "mdp not installed" 1>&2
 	[ -x "$$(command -v mdp)" ] && mdp README.md
 	chmod a-w $(GENERATED)
